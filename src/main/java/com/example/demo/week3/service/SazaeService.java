ca473af6dccce3d16f12b46d6d72a56627f7d9ca
shimizujpn <yudazoofficial@gmail.com>
2022-05-24 00:44:28 +0900

    サザエさんじゃんけん

--- src/main/java/com/example/demo/week3/service/SazaeService.java
+++ src/main/java/com/example/demo/week3/service/SazaeService.java
@@ -1,5 +1,9 @@
 package com.example.demo.week3.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,7 +25,21 @@ public class SazaeService {
      * @return result win,lose,draw のいずれかの文字列
      * @throws IllegalArgumentException myHand または opponentHand が goo,choki,per のいずれかでない場合
      */
-    public String janken(String myHand, String opponentHand) {
-
+    public String janken(String myHand, String opponentHand)throws IllegalArgumentException{
+           String[] jyanken = {"goo", "choki", "per"};
+           List<String>jyankenList = new ArrayList<String>(Arrays.asList(jyanken));
+           int myHandindex = jyankenList.indexOf(myHand);
+           int opponentHandindex = jyankenList.indexOf(opponentHand);
+           String result = null;
+           if(myHandindex == 0 && opponentHandindex ==1 || myHandindex == 1 && opponentHandindex == 2 || myHandindex == 2 && opponentHandindex == 0) {
+        	   result = "win";
+           }else if(myHandindex == 0 && opponentHandindex == 2 || myHandindex == 1 && opponentHandindex == 0 || myHandindex == 2 && opponentHandindex == 1) {
+        	   result = "lose";
+           }else if(myHandindex == opponentHandindex){
+        	   result = "draw";
+           }else if(myHandindex == -1 || opponentHandindex == -1) {
+        	   throw new IllegalArgumentException("goo,choki,perいずれかでない場合");
+           }
+return result;
     }
 }
